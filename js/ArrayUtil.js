a8a1e7b70a71ca67bfd2e9d6e248b5842776789f
TKaxv_7S <954067342@qq.com>
2018-07-05 18:46:32 +0800

    修复&更新

--- js/ArrayUtil.js
+++ js/ArrayUtil.js
@@ -1,5 +1,6 @@
 /*
 * @Author TKaxv_7S
+*
 */
 //当前页
 var thisPage;
@@ -10,15 +11,20 @@ var isPage;
 //生成条件
 var selArgs = {};
 
-function setArgs(LID, LTYP, OSCR, OTYP) {
+var setArgs = function (ALL, LID, LTYP, OSCR, OTYP) {
+    selArgs.ALL = ALL;
     selArgs.LID = LID;
     selArgs.LTYP = LTYP;
     selArgs.OSCR = OSCR;
     selArgs.OTYP = OTYP;
-}
+    //全选
+    $(selArgs.ALL).click(function () {
+        $(selArgs.LID).find("input[type='checkbox']").prop("checked", $(this).is(":checked"));
+    });
+};
 
 //生成JSON对象
-function setData(selArgs) {
+var setData = function () {
     //判断是否存在
     isPage = thisPage in postData;
     var List = MarkSaveList(selArgs);
@@ -29,34 +35,51 @@ function setData(selArgs) {
     } else {
         postData[thisPage] = List;
     }
-}
+    return postData;
+};
 
 //获得JSON对象数组
-function getData() {
-    setData(selArgs);
+var getData = function () {
+    setData();
     var List = [];
-    for (thePage in postData) {
+    for (var thePage in postData) {
         $(postData[thePage]).each(function (i, dom) {
             dom != null && List.push(dom);
         })
     }
     return List;
-}
+};
+
+//更新当前页
+function savePage(savePage) {
+    //记录当前页
+    thisPage = savePage;
+    //判断是否存在
+    isPage = thisPage in postData;
+    return isPage && postData[thisPage] || [];
+};
+
+//获取某页数据
+function getPageData(Page) {
+    //判断是否存在
+    isPage = Page in postData;
+    return isPage && postData[Page] || [];
+};
+
+//是否取消全选
+function cancelAll(cx) {
+    $(selArgs.ALL).prop("checked", !cx);
+};
 
 //重置
 function Reset() {
     postData = {};
-    setData(selArgs);
-}
+    // setData();
+};
 
-//是否需要全选
-/*
-function isChkAll(selector) {
-    $(selector).each(function (i, dom) {
-        if (!$(dom).is(":checked")) {
-            return false;
-        }
-    });
-    return true;
-}
-*/
+//全部重置
+function refresh() {
+    $(selArgs.ALL).prop("checked", false);
+    $(selArgs.LID).find("input[type='checkbox']").prop("checked", false);
+    Reset();
+};
